716f001131c686236393880866b999b9a3ac041f
Jerad Garland <72949161+jgarlandetsu@users.noreply.github.com>
2021-09-13 13:35:44 -0400

    Add files via upload
    This is the initial commit of Scrum Age to the repository.

---
+++ StoneAgeConsole/StoneAgeConsole/Die.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneAgeConsole
+{
+    class Die
+    {
+        List<String> DiceFaces = new List<string>();
+        String Face;
+
+        public Die(Random r1)
+        {
+            MakeDieFaces();
+            AssignFace(r1);
+            PrintDie();
+        }
+
+        public void MakeDieFaces()
+        {
+            DiceFaces.Add(" _________ \n|         |\n|         |\n|    X    |\n|         |\n|_________|\n");
+            DiceFaces.Add(" _________ \n|         |\n|  X      |\n|         |\n|      X  |\n|_________|\n");
+            DiceFaces.Add(" _________ \n|         |\n|  X      |\n|    X    |\n|      X  |\n|_________|\n");
+            DiceFaces.Add(" _________ \n|         |\n|  X   X  |\n|         |\n|  X   X  |\n|_________|\n");
+            DiceFaces.Add(" _________ \n|         |\n|  X   X  |\n|    X    |\n|  X   X  |\n|_________|\n");
+            DiceFaces.Add(" _________ \n|         |\n|  X   X  |\n|  X   X  |\n|  X   X  |\n|_________|\n");
+        }
+
+        public void AssignFace(Random r2)
+        {
+            Face = DiceFaces[r2.Next(6)];
+        }
+
+        public void PrintDie()
+        {
+            Console.Write(Face);
+        }
+    }
+}
---
+++ StoneAgeConsole/StoneAgeConsole/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneAgeConsole
+{
+    class Player
+    {
+        String PlayerName;
+        String PlayerNumber;
+        String PlayerColor;
+        int AgricultureTrack;
+        int ScoreTrack;
+
+        public Player(int playerNum)
+        {
+            Console.Clear();
+            PlayerNumber = "Player Number " + playerNum;
+            Console.WriteLine("Welcome Player " + playerNum + "!");
+            Console.WriteLine("What is your name? \n");
+            PlayerName = Console.ReadLine();
+        }
+
+        public String chooseColor(List<String> colorList)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello " + PlayerName + "!");
+            Console.WriteLine("\nWhich color would you like to play as? \nThese are your options:\n");
+
+            for(int x=0; x < colorList.Count; x++)
+            {
+                Console.WriteLine(colorList[x]);
+            }
+
+            Console.WriteLine("\n");
+
+            for(;;)
+            {
+                String userInput = Console.ReadLine();
+
+                if (colorList.Contains(userInput) == false)
+                {
+                    Console.WriteLine("That was not one of the options, please try again.");
+                }
+                else
+                {
+                    PlayerColor = userInput;
+                    return userInput;
+                }
+            }
+
+        }
+    }
+}
